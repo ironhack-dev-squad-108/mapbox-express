e10ccc4296b12c53d5f103efde93078fe6fbeaee
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-06 17:30:46 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const Court = require('../models/Court');
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/add-court', (req, res, next) => {
+  res.render('add-court')
+})
+
+// GOAL: take the values typed by the user; save a new Court; redirect the user to /
+router.post('/add-court', (req, res, next) => {
+  Court.create({
+    sport: req.body.sport,
+    location: {
+      type: 'Point', // this line is optional thanks to the model
+      coordinates: [req.body.lng, req.body.lat]
+    }
+  })
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(err => next(err))
+})
 
 module.exports = router;
